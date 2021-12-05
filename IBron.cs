1a299d34be152632400ff8e598ab3d2478a17812
PatrycjaSmol <54121426+PatrycjaSmol@users.noreply.github.com>
2021-12-05 11:38:42 +0100

    Add files via upload
    Dodanie interfejsu IBron

---
+++ IBron.cs
@@ -0,0 +1,12 @@
+﻿namespace gra_geekon
+{
+    public interface IBron
+    {
+        string Nazwa { get; set; }
+        int Cena { get; set; }
+        int Obrazenia { get; set; }
+        int ObliczObrazenia();
+        bool MozliwoscNoszeniaTarczy { get; }
+        
+    }
+}
