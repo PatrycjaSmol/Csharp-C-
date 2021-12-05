40a5398dedfc629d9981ec567d3a3f1e78cca937
PatrycjaSmol <54121426+PatrycjaSmol@users.noreply.github.com>
2021-12-05 11:39:31 +0100

    Add files via upload
    Dodanie możliwości wczytywania różnych rodzajów broni z pliku .txt

---
+++ Dane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace gra_geekon
+{
+    public class Dane
+    {
+        public List<IBron> WczytajBronie()
+        {
+            var bronie = new List<IBron>();
+
+            using (StreamReader reader = new StreamReader("bronie.txt"))
+            {
+                string linia;
+                while ((linia = reader.ReadLine()) != null)
+                {                                    
+                        var odczyt = linia.Split(';');
+                        bronie.Add(new Bron(odczyt[0], int.Parse(odczyt[1]), int.Parse(odczyt[2])));
+                        linia = reader.ReadLine();                 
+                }
+            }
+            using (StreamReader reader = new StreamReader("bronieDwureczne.txt"))
+            {
+                string linia;
+                while ((linia = reader.ReadLine()) != null)
+                {
+                    string[] odczyt = linia.Split(';');
+                    bronie.Add(new BronDwureczna(odczyt[0], int.Parse(odczyt[1]), int.Parse(odczyt[2])));
+                    linia = reader.ReadLine();
+                }
+            }
+
+            return bronie;
+        }
+    }
+}
