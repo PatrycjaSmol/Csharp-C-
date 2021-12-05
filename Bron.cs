370dc3b3f314b0608681305c58f0deb89a19cd99
PatrycjaSmol <54121426+PatrycjaSmol@users.noreply.github.com>
2021-12-05 11:37:42 +0100

    Add files via upload
    Dodanie klasy Bron

---
+++ Bron.cs
@@ -0,0 +1,31 @@
+﻿namespace gra_geekon
+{
+    public class Bron : IBron
+    {
+       
+        public string Nazwa { get; set; }
+        public int Cena { get; set; }
+        public int Obrazenia { get; set; }
+       
+    
+        public bool MozliwoscNoszeniaTarczy
+        { get
+            {
+                return true;
+            }
+        }
+
+        public Bron(string nazwa, int cena, int obrazenia)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Obrazenia = obrazenia;
+        }
+
+        public int ObliczObrazenia()
+        {
+            return Obrazenia;
+        }
+        
+    }
+}
