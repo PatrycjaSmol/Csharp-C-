55ad76e3bc97e194dce278bb1ad45c6500bbe0c6
PatrycjaSmol <54121426+PatrycjaSmol@users.noreply.github.com>
2021-12-05 11:38:12 +0100

    Add files via upload
    Dodanie klasy BronDwureczna (rózne obrazenia)

---
+++ BronDwureczna.cs
@@ -0,0 +1,30 @@
+﻿namespace gra_geekon
+{
+    public class BronDwureczna : IBron
+
+    {
+        public string Nazwa { get; set; }
+        public int Cena { get; set; }
+        public int Obrazenia { get; set; }
+
+        public BronDwureczna(string nazwa, int cena, int obrazenia)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Obrazenia = obrazenia;
+        }
+
+        public bool MozliwoscNoszeniaTarczy
+        { get
+            {
+                return false;
+            }
+
+        }
+        
+        public int ObliczObrazenia()
+        {
+            return Obrazenia * 2;
+        }
+    }
+}
